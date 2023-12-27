280f9de53e7889383e4e27e8ea138cec85940ec0
PrasadPonnada <PrasadPonnada09@gmail.com>
2023-12-27 12:45:46 +0530

    added hasura , graphqlm postgress

--- part-1-create-simple-server/server/src/index.ts
+++ part-1-create-simple-server/server/src/index.ts
@@ -7,16 +7,9 @@ const fs = require('fs');
 const schemaString = fs.readFileSync(path.join(__dirname, './schema.gql'), 'utf8');
 const schema = buildSchema(schemaString);
 
-const authMiddleware = (req, res, next) => {
-  req.authHeader = req.headers.authorization;
-  next();
-}
-
 const root = {
   getUser: ({ id }, req) => {
-    console.log(req.authHeader);
-    
-    // mongoose/prisma logic to get the user
+    console.log(req.authheader);
     if (id === '1') {
       return { id: '1', email: 'john.doe@example.com', firstname: 'John', lastname: 'Doe' };
     }
@@ -27,18 +20,13 @@ const root = {
     // mongoose/prisma logic to store the user
     return { id: '2', ...input };
   }
-};
-
+}
 const app = express();
-
-app.get("/healthcheck", (req: any, res: any) => {
-  res.json({"msg": "hi"});
-})
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
-  graphiql: true,
-}));
+  graphiql: true
+}))
 
 const PORT = 4000;
 app.listen(PORT, () => {
---
+++ part-2-hasura-dockerfile/hasura/migrations/default/1703623830017_create_table_public_Users_Test/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."Users_Test" ("id" integer NOT NULL, "firstname" text NOT NULL, "lastname" text NOT NULL, PRIMARY KEY ("id") );COMMENT ON TABLE "public"."Users_Test" IS E'Users_Test';
